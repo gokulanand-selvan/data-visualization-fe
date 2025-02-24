a4b2ea5644903d48904de0c04a1bcfe2c96a328d
gokulanandselvan <gokul2848@gmail.com>
2025-02-24 23:13:42 +0530

    active tab style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import HeartDisease from "./layout/Heart-Disease/HeartDisease";
 
 function App() {
+
+  const [activeSector, setActiveSector] = useState(0);
+
   const SectorTitle = [
     "Heart Disease",
     "Zomato Data",
@@ -16,7 +20,8 @@ function App() {
         {SectorTitle.map((title, ind) => (
           <button
             key={ind}
-            className="text-lg border p-2 rounded-lg font-medium cursor-pointer hover:bg-gray-200 "
+            className={`text-lg border p-2 rounded-lg font-medium cursor-pointer hover:bg-gray-200  ${activeSector === ind && "text-white bg-black font-medium hover:bg-gray-950 "} `}
+            onClick={() => setActiveSector(ind)}
           >
             {title}
           </button>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 export default function Header() {
   return (
-    <div className="px-2 py-6 bg-black text-white">
+    <div className="px-2 py-6 bg-black text-white flex justify-between items-center">
       <div className="text-2xl font-bold">Visualize Data</div>
+      <a
+        style={{ color: "white" }}
+        href="https://www.gokulanand.in" target="_blank" className="text-xl px-4 font-semibold ">About me</a>
     </div>
   );
 }
